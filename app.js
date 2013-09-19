5ffb90d569e72bc999773e6aa984a0cdf54af34e
sekka1 <garlandk@gmail.com>
2013-09-19 15:54:00 -0700

    Updated real streaming for statistic_eps_stat_device and auth call

--- app.js
+++ app.js
@@ -17,9 +17,14 @@ app.get('/*', function (req, res) {
 
 io.sockets.on('connection', function (socket) {
 
-  socket.on('algorithm_random_forest', function(data){
+    // Interval Vars
+    var interval_statistic_eps_stat_device;
+    var interval_statistic_eps_stat_auth_token;
+
+
+    socket.on('algorithm_random_forest', function(data){
         console.log('Running algorithm_random_forest');
-      
+
         algorithmsRandomForest.getResult(data, function(err,result){
             if(err){
                 socket.emit('algorithm_random_forest_result', { data: err });
@@ -29,10 +34,10 @@ io.sockets.on('connection', function (socket) {
         });
         //algorithms.run('randomForest', data);
         //algorithms.run('svm', data);
-  });
-  socket.on('algorithm_random_forest_rolling_samples', function(data){
+    });
+    socket.on('algorithm_random_forest_rolling_samples', function(data){
         console.log('Running algorithm_random_forest_rolling_samples');
-      
+
         algorithmsRandomForest.getResultRollingSamples(data, function(err,result){
             if(err){
                 socket.emit('algorithm_random_forest_rolling_samples_result', { data: err });
@@ -40,10 +45,10 @@ io.sockets.on('connection', function (socket) {
                 socket.emit('algorithm_random_forest_rolling_samples_result', { data: result });
             }
         });
-  });
-  socket.on('algorithm_svm', function(data){
+    });
+    socket.on('algorithm_svm', function(data){
         console.log('Running algorithm_svm');
-        
+
         algorithmsRandomForest.getResult(data, function(err,result){
             if(err){
                 socket.emit('algorithm_svm_result', { data: err });
@@ -51,10 +56,10 @@ io.sockets.on('connection', function (socket) {
                 socket.emit('algorithm_svm_result', { data: result });
             }
         });
-  });
-  socket.on('event_save', function(data){
+    });
+    socket.on('event_save', function(data){
         console.log('Running event_save');
-        
+
         // Save the event
         events.saveGeneric(data, function(err,result){
             if(err){
@@ -63,10 +68,10 @@ io.sockets.on('connection', function (socket) {
                 socket.emit('event_save_result', { data: result });
             }
         });
-  });
-  socket.on('event_save_motion', function(data){
+    });
+    socket.on('event_save_motion', function(data){
         console.log('Running event_save_motion');
-        
+
         // Save the event
         events.saveAccelerometerGyroscope(data, function(err,result){
             if(err){
@@ -75,10 +80,10 @@ io.sockets.on('connection', function (socket) {
                 socket.emit('event_save_motion_result', { data: result });
             }
         });
-  });
-  socket.on('query_get_data_by_range', function(data){
+    });
+    socket.on('query_get_data_by_range', function(data){
         console.log('Running query_get_data_by_range');
-        
+
         // Save the event
         query.get_data_by_range(data, function(err,result){
             if(err){
@@ -87,34 +92,44 @@ io.sockets.on('connection', function (socket) {
                 socket.emit('query_get_data_by_range', { data: result });
             }
         });
-  });
-  socket.on('statistic_eps_stat_device', function(data){
+    });
+    socket.on('statistic_eps_stat_device', function(data){
 
-        console.log('Running statistic_eps_stat_device');
+        // Run interval at 1 second
+        interval_statistic_eps_stat_device = setInterval(function(){
 
-        statistics.eps_stat_device(data, function(err,result){
-            if(err){
-                socket.emit('statistic_eps_stat_device', { data: err });
-            }else{
-                socket.emit('statistic_eps_stat_device', { data: result });
-            }
-        });
-  });
+            console.log('Running statistic_eps_stat_device');
+
+            statistics.eps_stat_device(data, function(err,result){
+                if(err){
+                    socket.emit('statistic_eps_stat_device', { data: err });
+                }else{
+                    socket.emit('statistic_eps_stat_device', { data: result });
+                }
+            });
+
+        }, 1000);
+
+    });
     socket.on('statistic_eps_stat_auth_token', function(data){
 
-        console.log('Running statistic_eps_stat_auth_token');
+        // Run interval at 1 second
+        interval_statistic_eps_stat_auth_token = setInterval(function(){
 
-        statistics.eps_stat_auth_token(data, function(err,result){
-            if(err){
-                socket.emit('statistic_eps_stat_auth_token', { data: err });
-            }else{
-                socket.emit('statistic_eps_stat_auth_token', { data: result });
-            }
-        });
-  });
-  socket.on('statistic_get_rolling_avg_and_std', function(data){
+            console.log('Running statistic_eps_stat_auth_token');
+
+            statistics.eps_stat_auth_token(data, function(err,result){
+                if(err){
+                    socket.emit('statistic_eps_stat_auth_token', { data: err });
+                }else{
+                    socket.emit('statistic_eps_stat_auth_token', { data: result });
+                }
+            });
+        }, 1000);
+    });
+    socket.on('statistic_get_rolling_avg_and_std', function(data){
         console.log('Running statistic_get_rolling_avg_and_std');
-        
+
         // Save the event
         statistics.get_rolling_avg_and_std(data, function(err,result){
             if(err){
@@ -123,7 +138,16 @@ io.sockets.on('connection', function (socket) {
                 socket.emit('statistic_get_rolling_avg_and_std', { data: result });
             }
         });
-  });
+    });
+
+    // User disconnection
+    socket.on('disconnect', function() {
+
+        console.log('Connection disconnected...');
+
+        // Run clean up for interval methods
+        clearInterval(interval_statistic_eps_stat_device);
+        clearInterval(interval_statistic_eps_stat_auth_token);
+    })
+
 });
-
-
